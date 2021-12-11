aa1f3247c5e03dcce2419733f586283b28552bf7
steven4program <stevenchang421@gmail.com>
2021-12-12 00:21:27 +0800

    test

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(methodOverride('_method'))
 app.use('/upload', express.static(__dirname + '/upload'))
 
 // load static resource
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'public')))
+// const path = require('path')
+// app.use(express.static(path.join(__dirname, 'public')))
 
 // Socket
 const server = require('http').createServer(app)
