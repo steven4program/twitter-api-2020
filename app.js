82fb11546281f06f29a6b5ffc55a76cff3879152
steven4program <stevenchang421@gmail.com>
2021-12-12 17:44:12 +0800

    test

--- app.js
+++ app.js
@@ -33,8 +33,43 @@ app.use('/upload', express.static(__dirname + '/upload'))
 // app.use(express.static(path.join(__dirname, 'public')))
 
 // Socket
-const server = require('http').createServer(app)
-require('./socket/socketio').socket(server)
+// const exphbs = require('express-handlebars')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+let users = []
+let messagesArr = []
+let index = 0
+const messages = [
+  {
+    name: 'Jack',
+    message: 'HELLOOOOO'
+  }
+]
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.emit('allMessages', messages)
+  socket.on('message', (obj) => {
+    console.log('使用者' + obj.name + '傳來訊息' + obj.message)
+    messages.push(obj)
+    io.emit('newMessage', obj)
+  })
+  // socket.on("mouseMove", obj => {
+  //   console.log(obj)
+  // })
+  // socket.emit("")
+  socket.on('newuser', (username) => {
+    // username來自前端進入聊天室的動作
+    console.log(`${username} has arrived the chatroom`)
+    socket.username = username
+    users.push(socket)
+  })
+  socket.on('disconnect', () => {
+    console.log(`${socket.username} has left the chatroom.`)
+  })
+})
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- socket/socketio.js
+++ socket/socketio.js
@@ -1,47 +1,47 @@
-const socketio = require('socket.io')
+// const socketio = require('socket.io')
 
-let io
-let userList = []
+// let io
+// let userList = []
 
-const socket = (server) => {
-  io = socketio(server, {
-    cors: {
-      origin: [
-        'https://learnpytest.github.io/Front_End_Vue_Simple_Twitter',
-        'http://localhost:3000',
-        'http://localhost:8080'
-      ],
-      methods: ['GET', 'POST'],
-      transports: ['websocket', 'polling'],
-      credentials: true
-    },
-    allowEIO3: true
-  })
-  console.log('Init success')
+// const socket = (server) => {
+//   io = socketio(server, {
+//     cors: {
+//       origin: [
+//         'https://learnpytest.github.io/Front_End_Vue_Simple_Twitter',
+//         'http://localhost:3000',
+//         'http://localhost:8080'
+//       ],
+//       methods: ['GET', 'POST'],
+//       transports: ['websocket', 'polling'],
+//       credentials: true
+//     },
+//     allowEIO3: true
+//   })
+//   console.log('Init success')
 
-  if (!io) throw new Error('Init fail')
+//   if (!io) throw new Error('Init fail')
 
-  io.on('connection', (socket) => {
-    console.log('connected')
-    socket.on('join', () => {
-      console.log('User join')
-      socket.broadcast.emit('new member', {
-        message: 'User join'
-      })
-    })
+//   io.on('connection', (socket) => {
+//     console.log('connected')
+//     socket.on('join', () => {
+//       console.log('User join')
+//       socket.broadcast.emit('new member', {
+//         message: 'User join'
+//       })
+//     })
 
-    socket.on('message', (msg) => {
-      console.log('msg', msg)
-      socket.broadcast.emit('new message', msg)
-    })
+//     socket.on('message', (msg) => {
+//       console.log('msg', msg)
+//       socket.broadcast.emit('new message', msg)
+//     })
 
-    socket.on('disconnect', () => {
-      console.log('User leave.')
-      io.emit('member leave', {
-        message: 'user leave'
-      })
-    })
-  })
-}
+//     socket.on('disconnect', () => {
+//       console.log('User leave.')
+//       io.emit('member leave', {
+//         message: 'user leave'
+//       })
+//     })
+//   })
+// }
 
-module.exports = { socket }
+// module.exports = { socket }
